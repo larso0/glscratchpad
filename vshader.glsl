7bab45fa423549b3c1811c8d9bf23ab7903eec49
larso0 <lars.olav.tolo@gmail.com>
2015-11-23 19:32:19 +0100

    Removed unused rotate function. Use only quaternion to rotate

--- vshader.glsl
+++ vshader.glsl
@@ -9,25 +9,12 @@ uniform mat4 projection;
 
 out vec3 modelposition;
 
-vec3 mrotate(vec3 vec, vec3 axis, float angle)
-{
-    axis = normalize(axis);
-    float s = sin(angle);
-    float c = cos(angle);
-    float oc = 1.0 - c;
-    
-    return mat3(oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,
-                oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,
-                oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c)
-                *vec;
-}
-
 vec3 qtransform( vec4 q, vec3 v )
 { 
 	return v + 2.0*cross(cross(v, q.xyz ) + q.w*v, q.xyz);
 }
 
-vec3 qrotate(vec3 vec, vec3 axis, float angle)
+vec3 rotate(vec3 vec, vec3 axis, float angle)
 {
 	axis = normalize(axis);
 	vec4 quat = vec4(sin(angle/2)*axis, cos(angle/2));
@@ -38,5 +25,5 @@ void main()
 {
 	modelposition = position;
 	gl_Position = projection * modelview *
-				  vec4(qrotate(position, rotation_axis, rotation_angle), 1);
+				  vec4(rotate(position, rotation_axis, rotation_angle), 1);
 }
